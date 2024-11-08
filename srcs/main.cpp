22110ef410211baa0fe5515d0cbdceab876c79ef
TonUpMan <qdevianne@gmail.com>
2024-11-08 15:46:29 +0100

    begin/comprehension ++

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,26 +4,35 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <cstdlib>
 
 int	main(int ac, char **av){
-	if(ac == 3){
-		struct sockaddr_in *adr;
+	(void)av;
+	(void)ac;
 
-		(void)av;
-		int s = socket(AF_INET, SOCK_STREAM, 0); //creation d'un point d'acces
+		struct sockaddr_in adr;
 
-		adr.sin_family = AF_INET;			/////////////////////////////////////////////////////////////////////
-		adr.sin_addr.s_addr = INADDDR_ANY;  //stockage des info(port, type) dans une struct?? en cpp??///////////
-		adr.sin_port = hton(8080);			/////////////////////////////////////////////////////////////////////
+		int s = socket(AF_INET, SOCK_STREAM, 0); 		//creation d'un point d'acces
+		adr.sin_family = AF_INET;						////////////////////////////////////////////////////////////
+		adr.sin_addr.s_addr = INADDR_ANY;  				///// stockage des info(port, type) dans une struct ////////
+		adr.sin_port = htons(8080);						////////////////////////////////////////////////////////////
 		
-		bind(s, &adr, sizeof(adr)); //adressage du point d'acces
+		bind(s, (struct sockaddr*)&adr, sizeof(adr)); //adressage du point d'acces
+		listen(s, 5); //mise en "ecoute" pour connection 2 = max queue length
 		
-		listen(s, 2); //mise en "ecoute" pour connection
-		
-		select() // connection...
+		//select(); // connection...
+/*	
 
-	}
+	*	En général, pour les communications réseau, C++ offre des fonctions comme 
+		-htonl() (host-to-network long) 
+		-htons() (host-to-network short)
+		pour convertir les valeurs de l'ordre local de l'hôte (qui peut être little-endian sur un PC classique) vers big-endian,
+
+	*	fcntl() is used to set the O_NONBLOCK flag on the server socket file descriptor.
+		This flag sets the socket to non-blocking mode
+
+*/
+
 	return (0);
 }
